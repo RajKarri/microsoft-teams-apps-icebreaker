96b8cf83b37ac9e935e9ebf482b52b6dc5dbb016
Raj Karri <rajkarri85@gmail.com>
2023-05-01 18:59:59 -0500

    Update PairUpNotificationAdaptiveCard.cs

--- Source/Icebreaker/Helpers/AdaptiveCards/PairUpNotificationAdaptiveCard.cs
+++ Source/Icebreaker/Helpers/AdaptiveCards/PairUpNotificationAdaptiveCard.cs
@@ -33,10 +33,18 @@ namespace Icebreaker.Helpers.AdaptiveCards
         /// <param name="teamName">The team name</param>
         /// <param name="botInstaller">The name of the person that installed the bot</param>
         /// <returns>The welcome team adaptive card</returns>
-        public static Attachment GetCard(string teamName, string botInstaller)
+        // public static Attachment GetCard(string teamName, string botInstaller)
+        public static Attachment GetCard(string teamName, TeamsChannelAccount sender, TeamsChannelAccount recipient, string botInstaller)
         {
             // Set alignment of text based on default locale.
             var textAlignment = CultureInfo.CurrentCulture.TextInfo.IsRightToLeft ? AdaptiveHorizontalAlignment.Right.ToString() : AdaptiveHorizontalAlignment.Left.ToString();
+
+            // Guest users may not have their given name specified in AAD, so fall back to the full name if needed
+            var senderGivenName = string.IsNullOrEmpty(sender.GivenName) ? sender.Name : sender.GivenName;
+            var recipientGivenName = string.IsNullOrEmpty(recipient.GivenName) ? recipient.Name : recipient.GivenName;
+
+            // To start a chat with a guest user, use their external email, not the UPN
+            var recipientUpn = !IsGuestUser(recipient) ? recipient.UserPrincipalName : recipient.Email;
 
             string teamIntroPart1;
             string teamIntroPart2;
@@ -65,6 +73,7 @@ namespace Icebreaker.Helpers.AdaptiveCards
                 teamIntroPart1,
                 teamIntroPart2,
                 teamIntroPart3,
+                personUpn = recipientUpn,
                 welcomeCardImageUrl = $"https://{baseDomain}/Content/welcome-card-image.png",
                 tourUrl = GetTourFullUrl(appId, GetTourUrl(baseDomain), tourTitle),
                 salutationText = Resources.SalutationTitleText,
@@ -73,5 +82,15 @@ namespace Icebreaker.Helpers.AdaptiveCards
 
             return GetCard(AdaptiveCardTemplate.Value, welcomeData);
         }
+
+        /// <summary>
+        /// Checks whether or not the account is a guest user.
+        /// </summary>
+        /// <param name="account">The <see cref="TeamsChannelAccount"/> user to check.</param>
+        /// <returns>True if the account is a guest user, false otherwise.</returns>
+        private static bool IsGuestUser(TeamsChannelAccount account)
+        {
+            return account.UserPrincipalName.IndexOf(ExternallyAuthenticatedUpnMarker, StringComparison.InvariantCultureIgnoreCase) >= 0;
+        }
     }
-    }
+}
